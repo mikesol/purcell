9a0a10e0930a9201077543be5dc27ece3c0c12b6
Mirjam Schulman <mirjam_schulman@hotmail.com>
2014-08-04 12:55:18 +0300

    Removes extraneous holder test. Push test from remote linux machine.

--- horstemps_next_to_horstemps_x_offset.py
+++ horstemps_next_to_horstemps_x_offset.py
@@ -2,10 +2,6 @@ from sqlalchemy.sql.expression import literal, distinct, exists, text, case
 from plain import *
 import time
 
-class _Holder(object) : 
- def __init__(self, id) :
-   self.id = id
-
 class _Delete(DeleteStmt) :
   def __init__(self, time_next, local_onset, start_with_id) :
     def where_clause_fn(id) :
@@ -123,21 +119,9 @@ if __name__ == "__main__" :
     # creates strains
     if x not in [a - b for a in [y * 2**(EXP - 2) for y in range(1,5)] for b in range(1,3)] :
       stmts.append((Time_next, {'id':x, 'val':x + 2}))
-      #TIME_NEXT[x] = _Holder(x + 2)
     stmts.append((Duration_log, {'id':x, 'val':0}))
     stmts.append((Duration, {'id':x, 'num':((x % 3) + 1), 'den':4}))
-    #DURATIONS.append((((x % 3) + 1), 4))
 
-  '''
-  def populate(key, tn, d, v=0) :
-    if tn.has_key(key) :
-      onset = v + d[key]
-      tn[key].onset = onset
-      populate(tn[key].id, tn, d, onset)
-
-  for x in XX | YY :
-    populate(x, TIME_NEXT, DURATIONS)
-  '''
   trans = conn.begin()
   for st in stmts :
     manager.insert(conn, st[0], st[1], MANUAL_DDL)
